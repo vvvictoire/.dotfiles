bccdc2d5631babfb988b713c5b35ac757875eeec
Erwan Milis <erwan@milis.fr>
2016-12-03 12:09:50 +0100

    Better install script

--- install.sh
+++ install.sh
@@ -1,4 +1,31 @@
 #!/bin/zsh
 
+# Testing if files already exists
+
+if [ -e ~/.zshrc ]
+then
+    echo "~/.zshrc already exists, do you want to remove it?"
+    select yn in "Yes" "No";
+    do
+        case $yn in
+            Yes ) rm ~/.zshrc; break;;
+            No ) break;;
+        esac
+    done
+fi
+
+if [ -e ~/.vimrc ]
+then
+    echo "~/.vimrc already exists, do you want to remove it?"
+    select yn in "Yes" "No";
+    do
+        case $yn in
+            Yes ) rm ~/.vimrc; break;;
+            No ) break;;
+        esac
+    done
+fi
+
+
 ln -s ~/.trucy_config/.zshrc ~/.zshrc
 ln -s ~/.trucy_config/.vimrc ~/.vimrc
