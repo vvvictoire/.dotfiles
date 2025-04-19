7c82a100c9ba40950e7e1cded4f44b05b57404e9
trucy <vvictoire@proton.me>
2025-04-19 22:18:33 +0200

    uh

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -2,15 +2,15 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
-    init = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = { "clangd" }
-        })
-        require('lspconfig').clangd.setup({
-            cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
-            init_options = {
-                fallbackFlags = { '-std=c++11' },
-            },
-        })
-    end
+--    init = function()
+--        require'lspconfig'.rust_analyzer.setup{
+--            settings = {
+--                ['rust-analyzer'] = {
+--                  diagnostics = {
+--                      enable = false;
+--                  }
+--              }
+--          }
+--      }
+--  end
 }
