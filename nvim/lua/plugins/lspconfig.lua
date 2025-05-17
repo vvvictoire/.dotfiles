b01391ad6e53eaad260b1f7bb312542342efaa53
vvvictoire <vvictoire@proton.me>
2025-05-17 19:55:36 +0200

    clean nvim plugins

--- nvim/lua/plugins/lspconfig.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "neovim/nvim-lspconfig",
-    init = function()
-        require'lspconfig'.pyright.setup{}
-        require'lspconfig'.clangd.setup{}
-    end
-}
--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,22 +1,25 @@
 return {
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    --vim.g.vimtex_view_method = "atril"
-    vim.g.vimtex_view_general_viewer = "xdg-open"
-    --[[vim.g.vimtex_compiler_latexmk = {
-        executable = 'latexmk',
-        options = {
-            '-xelatex',
-            '-file-line-error',
-            '-synctex=1',
-            '-interaction=nonstopmode',
-        }
-    }--]]
-    vim.g.vimtex_compiler_latexmk_engines = { _ = '-xelatex' }
-    vim.g.vimtex_mappings_enabled = false
-    vim.keymap.set('n', '<localleader>ll' , '<plug>(vimtex-compile)')
-  end
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+        -- VimTeX configuration goes here, e.g.
+        --vim.g.vimtex_view_method = "atril"
+        vim.g.vimtex_view_general_viewer = "xdg-open"
+        --[[vim.g.vimtex_compiler_latexmk = {
+            executable = 'latexmk',
+            options = {
+                '-xelatex',
+                '-file-line-error',
+                '-synctex=1',
+                '-interaction=nonstopmode',
+            }
+        }--]]
+        vim.g.vimtex_compiler_latexmk_engines = { _ = '-xelatex' }
+        --vim.g.vimtex_mappings_enabled = false
+        vim.keymap.set('n', '<leader>ll' , '<plug>(vimtex-compile)')
+    end,
+    config = function()
+        --vim.g.maplocalleader=" "
+    end
 }
