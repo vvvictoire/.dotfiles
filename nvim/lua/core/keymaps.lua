43be3c069704b4859e5202c3deebd4f54949eeaf
vvvictoire <vvictoire@proton.me>
2025-06-05 17:54:05 +0200

    fix

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,7 +1,3 @@
 local keymap = vim.keymap.set
-<<<<<<< HEAD
 
 keymap("n", "Z", "<cmd>:w<cr>")
---  keymap("i", "  ", "<ESC>", {desc = "Exit insert mode with tt"})
-=======
->>>>>>> refs/remotes/origin/master
